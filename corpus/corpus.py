1a6af1b96a6806dcef9882f102d70f69a15d2f93
Matthew Pabst <pabstmatthew@gmail.com>
2021-09-28 18:20:20 -0500

    added caching

--- corpus/corpus.py
+++ corpus/corpus.py
@@ -3,22 +3,37 @@ sys.path.append('/home/matthewp/texas-papers')
 
 import pytesseract
 from urllib.request import urlretrieve
+from urllib.parse import urlparse
 import os
 from utils.utils import *
 
 def img2txt(url, cleanup=True):
-    dbg_start('Downloading file')
-    fname, _ = urlretrieve(url)
+    scope = 'OCRText'
+    fname = urlparse(url).path[1:].replace('/', '_')
+
+    # Check the cache.
+    cached_txt = cache_read(scope, fname)
+    if cached_txt:
+        return cached_txt
+
+    # Download the file.
+    dbg_start('Downloading file "{}"'.format(fname))
+    urlretrieve(url, fname)
     dbg_end()
+
+    # Run the OCR.
     try:
         dbg_start('Starting OCR')
         txt = pytesseract.image_to_string(fname)
-        dbg_end('Done!')
+        dbg_end()
     except pytesseract.pytesseract.TesseractNotFoundError:
         print('You need to install tesseract with your package manager!')
-        txt = ''
+
+    # Cleanup and cache the result.
     if cleanup:
         os.remove(fname)
+    if txt != '':
+        cache_write(scope, fname, txt)
     return txt
 
 if __name__ == '__main__':
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import pickle as pkl
 
 DEBUG = True
 
@@ -26,4 +28,29 @@ def err(msg):
 
 def warn(msg):
     print(RED+BOLD+'[WARN] '+END)
+
+
+CACHE_PATH = os.path.expanduser('~/.cache/texas-papers')
+
+def cache_write(scope, name, data, flush=False):
+    # Make sure the cache exists.
+    if not os.path.exists(CACHE_PATH):
+        os.mkdir(CACHE_PATH)
+    # Form the path to this cached file.
+    fpath = os.path.join(CACHE_PATH, scope+'-'+name)
+    # If flushing, make sure the old file is gone.
+    if flush and os.path.exists(fpath):
+        os.remove(fpath)
+    # Cache the file.
+    if not os.path.exists(fpath):
+        with open(fpath, 'wb') as f:
+            pkl.dump(data, f)
+
+def cache_read(scope, name):
+    fpath = os.path.join(CACHE_PATH, scope+'-'+name)
+    if os.path.exists(fpath):
+        with open(fpath, 'rb') as f:
+            return pkl.load(f)
+    else:
+        return None
 
