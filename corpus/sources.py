6c79af512e6b6b27da495bef6d9b8e32810785b9
Matthew Pabst <pabstmatthew@gmail.com>
2021-09-28 16:37:07 -0500

    added dir for corpuses

---
+++ corpus/sources.py
@@ -0,0 +1,38 @@
+papers = {
+    'Austin Statesman ': [],
+}
+
+'''
+Marshall:       1849-1869
+    (1K issues)
+Dallas:         1855-1885, 1919-1922
+    (5K issues)
+Austin:         1871-1898
+    (1.2K issues)
+Waco:           1874-1889, 1892-1894
+    (3K issues)
+San Saba:       1876-1892
+    (0.4K issues)
+San Marcos:     1877-1890
+    (0.6K issues)
+San Antonio:    1883-1886
+    (0.8K issues)
+Fort Worth:     1883-1896
+    (4K issues)
+Brownsville:    1892-1909, 1912-1937
+    (13K issues)
+Houston:        1893-1903
+    (2K issues)
+Shiner:         1893-1921
+    (0.7K issues)
+Bryan:          1895-1917, 1892-1913
+    (6K issues)
+El Paso:        1896-1901, 1910-1921
+    (4.6K issues)
+Lubbock:        1908-1922
+    (0.4K issues)
+Corpus Christi: 1911-1921
+    (2K issues)
+Amarillo:       1911-1922, 1907-1920, 1905-1908
+    (1.7K issues)
+'''
